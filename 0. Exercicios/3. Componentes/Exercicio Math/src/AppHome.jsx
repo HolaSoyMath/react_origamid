393fce0f6e5363adf4dbe3277c43c20ba79cc7b5
Matheus Guimarães <82780534+HolaSoyMath@users.noreply.github.com>
2024-08-07 12:01:58 -0300

    inclusão exercicios

---
+++ 0. Exercicios/3. Componentes/Exercicio Math/src/AppHome.jsx
@@ -0,0 +1,12 @@
+import Menu from "./Menu/Menu";
+
+function AppHome() {
+  return (
+    <div>
+      <Menu titulo='Home' />
+      <p>Essa é a home do site</p>
+    </div>
+  );
+}
+
+export default AppHome;
